9cde1f1a93d192810095ab06deeb2c19ae6d9403
CharlesCZ <karakankaczyn@gmail.com>
2019-02-19 15:15:24 +0100

    Owner pettype fix.#Closes 59.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package guru.springframework.sfgpetclinic.controllers;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @RequestMapping("/owners")
@@ -25,6 +28,15 @@ public class OwnerController {
 
         return "owners/index";
     }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable Long ownerId){
+        ModelAndView mav=new ModelAndView("owners/ownerDetails");
+        mav.addObject("owner",ownerService.findByID(ownerId));
+        return mav;
+    }
+
+
 
     @RequestMapping("/find")
     public String findOwners(){
